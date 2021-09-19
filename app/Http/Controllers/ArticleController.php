3f740742bea551e25d0c53869c841e3612e40607
masatakamaekawa <mstkmekw@hito-koto.com>
2021-09-20 08:43:57 +0900

    bladeまで作成済

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+         // indexページへ移動
+     public function index()
+     {
+         // モデル名::テーブル全件取得
+         $articles = Article::all();
+         // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
+         return view('articles.index', ['articles' => $articles]);
+     }
+
+     // showページへ移動
+     public function show($id)
+     {
+         $article = Article::find($id);
+         return view('articles.show', ['article' => $article]);
+     }
 }
