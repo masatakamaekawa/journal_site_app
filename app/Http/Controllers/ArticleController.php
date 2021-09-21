1918d6476de808d0c3e6714f538f578eb8bc68c8
masatakamaekawa <mstkmekw@hito-koto.com>
2021-09-21 23:43:58 +0900

    課題2完了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,39 +8,39 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
          // indexページへ移動
-     public function index()
-     {
+    public function index()
+    {
          // モデル名::テーブル全件取得
-         $articles = Article::all();
+        $articles = Article::all();
          // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
-         return view('articles.index', ['articles' => $articles]);
-     }
+        return view('articles.index', ['articles' => $articles]);
+    }
 
      // showページへ移動
-     public function show($id)
-     {
-         $article = Article::find($id);
-         return view('articles.show', ['article' => $article]);
-     }
+    public function show($id)
+    {
+        $article = Article::find($id);
+        return view('articles.show', ['article' => $article]);
+    }
 
     public function create()
-     {
-         return view('articles.create');
-     }
+    {
+        return view('articles.create');
+    }
 
-     public function store(Request $request)
-     {
+    public function store(Request $request)
+    {
          // インスタンスの作成
-         $article = new Article;
+        $article = new Article;
 
          // 値の用意
-         $article->name = $request->name;
-         $article->writing = $request->writing;
+        $article->name = $request->name;
+        $article->writing = $request->writing;
 
          // インスタンスに値を設定して保存
-         $article->save();
+        $article->save();
 
          // 登録したらindexに戻る
-         return redirect('/article');
-     }
+        return redirect('/article');
+    }
 }
